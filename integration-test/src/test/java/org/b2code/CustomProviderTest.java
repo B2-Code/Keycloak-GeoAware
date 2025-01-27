a7727d1ef0cf157918cec9a2071c5fe1d67a0044
Benedikt Rohlf <benedikt.rohlf@gmx.de>
2025-01-27 18:28:30 +0100

    refactor: update CustomProviderTest to use lifecycle management and simplify client assertions

--- integration-test/src/test/java/org/b2code/CustomProviderTest.java
+++ integration-test/src/test/java/org/b2code/CustomProviderTest.java
@@ -4,33 +4,20 @@ import org.b2code.config.RealmAConfig;
 import org.b2code.config.ServerConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.resource.RealmResource;
-import org.keycloak.representations.idm.ClientRepresentation;
-import org.keycloak.testframework.annotations.InjectAdminClient;
 import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.injection.LifeCycle;
 import org.keycloak.testframework.realm.ManagedRealm;
-
-import java.util.List;
 
 @KeycloakIntegrationTest(config = ServerConfig.class)
 public class CustomProviderTest {
 
-    @InjectRealm(config = RealmAConfig.class)
+    @InjectRealm(lifecycle = LifeCycle.CLASS, config = RealmAConfig.class)
     private ManagedRealm realm;
 
-    @InjectAdminClient
-    Keycloak adminClient;
-
-    @Test
-    public void testCreateRealm() {
-        Assertions.assertEquals("realmA", realm.getName());
-    }
-    
     @Test
     public void testCreatedClient() {
-        RealmResource realmResource = adminClient.realm(realm.getName());
-        List<ClientRepresentation> clients = realmResource.clients().findAll();
+        Assertions.assertEquals(realm.getCreatedRepresentation().getClients().size(), 1);
+        Assertions.assertEquals(realm.getCreatedRepresentation().getClients().get(0).getId(), "test-client");
     }
 }

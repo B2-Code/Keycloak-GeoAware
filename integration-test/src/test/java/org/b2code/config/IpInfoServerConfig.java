bf4c333952999726f2543eb5033238f6b2ea45ca
BenLad17 <github@benlad.de>
2025-02-02 19:28:16 +0100

    PoC test multiple providers

---
+++ integration-test/src/test/java/org/b2code/config/IpInfoServerConfig.java
@@ -0,0 +1,22 @@
+package org.b2code.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IpInfoServerConfig extends ServerConfig {
+
+    private static final String PROVIDER_ID = "ipinfo-webservice";
+
+    private static final String TOKEN = System.getenv("IPINFO_TOKEN");
+
+    @Override
+    Map<String, String> getOptions() {
+        if (TOKEN == null) {
+            throw new IllegalStateException("IPINFO_TOKEN must be set as environment variable");
+        }
+        Map<String, String> options = new HashMap<>();
+        options.put("spi-geoaware-global-geoip-provider", PROVIDER_ID);
+        options.put("spi-geoaware-global-ipinfo-token", TOKEN);
+        return options;
+    }
+}

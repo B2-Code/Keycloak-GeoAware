05e7c485bcc066fbc9d75b98d4ca544c332b4352
BenLad17 <github@benlad.de>
2025-02-06 15:47:54 +0100

    enhance test flow setup with logins and config

--- integration-test/src/test/java/org/b2code/authentication/UnknownIpAuthenticatorProviderTest.java
+++ integration-test/src/test/java/org/b2code/authentication/UnknownIpAuthenticatorProviderTest.java
@@ -1,58 +1,19 @@
 package org.b2code.authentication;
 
-import jakarta.ws.rs.core.Response;
-import org.b2code.config.UnknownIpServerConfig;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.keycloak.representations.idm.AuthenticationExecutionExportRepresentation;
-import org.keycloak.representations.idm.AuthenticationFlowRepresentation;
+import org.b2code.config.MaxmindGeoLiteFileServerConfig;
+import org.keycloak.representations.idm.AuthenticationExecutionRepresentation;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
 
-import java.util.List;
-
-@KeycloakIntegrationTest(config = UnknownIpServerConfig.class)
+@KeycloakIntegrationTest(config = MaxmindGeoLiteFileServerConfig.class)
 public class UnknownIpAuthenticatorProviderTest extends BaseAuthenticatorProviderTest {
 
-    private static final String FLOW_NAME = "browser-with-unknown-ip";
     private static final String PROVIDER_ID = "geoaware-unknown-ip";
 
     @Override
-    @Test
-    void testCreateFlowWithExecution() {
-        createFlow();
-        Assertions.assertNotNull(realm.admin().flows().getFlow(FLOW_NAME));
-
-        AuthenticationFlowRepresentation authenticationFlowRepresentation = realm.admin().flows().getFlow(FLOW_NAME);
-        Assertions.assertNotNull(authenticationFlowRepresentation);
-
-        Assertions.assertEquals(0, authenticationFlowRepresentation.getAuthenticationExecutions().size());
-
-        authenticationFlowRepresentation.setAuthenticationExecutions(createExecutions());
-
-        Assertions.assertEquals(1, authenticationFlowRepresentation.getAuthenticationExecutions().size());
-    }
-
-    @Override
-    void createFlow() {
-        AuthenticationFlowRepresentation flow = new AuthenticationFlowRepresentation();
-        flow.setId(FLOW_NAME);
-        flow.setAlias(FLOW_NAME);
-        flow.setProviderId(FLOW_NAME);
-        flow.setTopLevel(true);
-        flow.setBuiltIn(false);
-
-        try (Response response = realm.admin().flows().createFlow(flow)) {
-            Assertions.assertEquals(201, response.getStatus());
-        }
-    }
-
-    @Override
-    List<AuthenticationExecutionExportRepresentation> createExecutions() {
-        AuthenticationExecutionExportRepresentation execution = new AuthenticationExecutionExportRepresentation();
-        execution.setAuthenticator(PROVIDER_ID);
-        execution.setRequirement("REQUIRED");
-        execution.setAuthenticatorFlow(true);
-
-        return List.of(execution);
+    AuthenticationExecutionRepresentation getAuthenticatorToTest() {
+        AuthenticationExecutionRepresentation authenticatorToTest = new AuthenticationExecutionRepresentation();
+        authenticatorToTest.setAuthenticator(PROVIDER_ID);
+        authenticatorToTest.setRequirement("REQUIRED");
+        return authenticatorToTest;
     }
 }

64011209a842aa149eb8049aeb20562831575ebe
Benedikt Rohlf <benedikt.rohlf@gmx.de>
2025-02-15 19:35:51 +0100

    add email notification tests for device and IP authentication

--- integration-test/src/test/java/org/b2code/authentication/UnknownIpAuthenticatorProviderTest.java
+++ integration-test/src/test/java/org/b2code/authentication/UnknownIpAuthenticatorProviderTest.java
@@ -1,6 +1,9 @@
 package org.b2code.authentication;
 
+import jakarta.mail.internet.MimeMessage;
 import org.b2code.config.MaxmindGeoLiteFileServerConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.keycloak.representations.idm.AuthenticationExecutionRepresentation;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
 
@@ -15,5 +18,53 @@ class UnknownIpAuthenticatorProviderTest extends BaseAuthenticatorProviderTest {
         authenticatorToTest.setAuthenticator(PROVIDER_ID);
         authenticatorToTest.setRequirement("REQUIRED");
         return authenticatorToTest;
+    }
+
+    @Test
+    public void testSendAlwaysIpEmail() throws Exception {
+        MimeMessage lastReceivedMessage;
+        setConditionAndAction("Always", "Notification Email (IP)");
+
+        login();
+        mailServer.waitForIncomingEmail(1);
+        Assertions.assertEquals(1, mailServer.getReceivedMessages().length);
+        lastReceivedMessage = mailServer.getLastReceivedMessage();
+        Assertions.assertEquals("New login alert", lastReceivedMessage.getSubject());
+        logout();
+
+        login();
+        mailServer.waitForIncomingEmail(1);
+        Assertions.assertEquals(2, mailServer.getReceivedMessages().length);
+        lastReceivedMessage = mailServer.getLastReceivedMessage();
+        Assertions.assertEquals("New login alert", lastReceivedMessage.getSubject());
+        logout();
+    }
+
+    @Test
+    public void testSendNeverNewIpEmail() throws Exception {
+        setConditionAndAction("Never", "Notification Email (IP)");
+
+        login();
+        mailServer.waitForIncomingEmail(0);
+        Assertions.assertEquals(0, mailServer.getReceivedMessages().length);
+        logout();
+    }
+
+    @Test
+    public void testUnknownIpEmail() throws Exception {
+        MimeMessage lastReceivedMessage;
+        setConditionAndAction("Unknown IP", "Notification Email (IP)");
+
+        login();
+        mailServer.waitForIncomingEmail(1);
+        Assertions.assertEquals(1, mailServer.getReceivedMessages().length);
+        lastReceivedMessage = mailServer.getLastReceivedMessage();
+        Assertions.assertEquals("New login alert", lastReceivedMessage.getSubject());
+        logout();
+
+        login();
+        mailServer.waitForIncomingEmail(0);
+        Assertions.assertEquals(1, mailServer.getReceivedMessages().length);
+        logout();
     }
 }

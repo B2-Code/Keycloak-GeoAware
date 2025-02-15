87f9409eb4e8f11ad7fbbbfcb8e48a63ff89e3f6
BenLad17 <48922322+BenLad17@users.noreply.github.com>
2025-02-15 18:59:00 +0100

    move duplicated code to superclass (#61)

--- provider/src/main/java/org/b2code/geoip/ipinfo/IpInfoProvider.java
+++ provider/src/main/java/org/b2code/geoip/ipinfo/IpInfoProvider.java
@@ -1,6 +1,5 @@
 package org.b2code.geoip.ipinfo;
 
-import com.google.common.base.Stopwatch;
 import io.ipinfo.api.IPinfo;
 import io.ipinfo.api.errors.RateLimitedException;
 import io.ipinfo.api.model.IPResponse;
@@ -9,8 +8,7 @@ import org.b2code.geoip.GeoIpInfo;
 import org.b2code.geoip.cache.CachingGeoIpProvider;
 import org.keycloak.models.KeycloakSession;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.util.Optional;
 
 @JBossLog
 public class IpInfoProvider extends CachingGeoIpProvider {
@@ -22,39 +20,21 @@ public class IpInfoProvider extends CachingGeoIpProvider {
         this.ipInfo = ipInfo;
     }
 
-    public GeoIpInfo getIpInfoImpl(String ipAddress) {
+    public Optional<GeoIpInfo> getIpInfoImpl(String ipAddress) {
         if (null == ipInfo) {
             log.warn("IpInfo GeoIP provider not initialized");
-            return GeoIpInfo.builder().ip(ipAddress).build();
+            return Optional.empty();
         }
-
-        InetAddress inetAddress = getInetAddress(ipAddress);
-        if (null == inetAddress || inetAddress.isSiteLocalAddress() || inetAddress.isLoopbackAddress()) {
-            log.debugf("Skipping GeoIP lookup for IP address '%s'", ipAddress);
-            return GeoIpInfo.builder().ip(ipAddress).build();
-        }
-
         try {
-            Stopwatch stopwatch = Stopwatch.createStarted();
             IPResponse ipInfoResponse = ipInfo.lookupIP(ipAddress);
-            log.debugf("IpInfo GeoIP lookup took %s", stopwatch.stop());
-            return IpInfoHelper.map(ipInfoResponse, ipAddress);
+            return Optional.ofNullable(IpInfoHelper.map(ipInfoResponse, ipAddress));
         } catch (RateLimitedException e) {
             log.error("Error while performing GeoIP lookup, because rate limit of IpInfo is reached", e);
         } catch (Exception e) {
             log.error("Error while performing GeoIP lookup", e);
         }
 
-        return GeoIpInfo.builder().ip(ipAddress).build();
-    }
-
-    private InetAddress getInetAddress(String ipAddress) {
-        try {
-            return InetAddress.getByName(ipAddress);
-        } catch (UnknownHostException e) {
-            log.errorf("Failed to resolve IP address '%s'", ipAddress, e);
-            return null;
-        }
+        return Optional.empty();
     }
 
 }

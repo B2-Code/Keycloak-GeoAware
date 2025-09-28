e476eb0caead75913aadc77f93e789b55bd7eb3d
BenLad17 <48922322+BenLad17@users.noreply.github.com>
2025-09-28 17:14:25 +0200

    refactor user agent info mapping to use DeviceActivityManager (#100)

--- provider/src/main/java/org/b2code/mapper/UserAgentInfoMapper.java
+++ provider/src/main/java/org/b2code/mapper/UserAgentInfoMapper.java
@@ -3,7 +3,8 @@ package org.b2code.mapper;
 import com.google.auto.service.AutoService;
 import lombok.extern.jbosslog.JBossLog;
 import org.b2code.PluginConstants;
-import org.keycloak.device.DeviceRepresentationProvider;
+import org.b2code.loginhistory.LoginRecord;
+import org.keycloak.device.DeviceActivityManager;
 import org.keycloak.models.ClientSessionContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.ProtocolMapperModel;
@@ -13,6 +14,7 @@ import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
 import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.representations.IDToken;
+import org.keycloak.representations.account.DeviceRepresentation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,8 +35,13 @@ public class UserAgentInfoMapper extends AbstractOIDCProtocolMapper implements A
     @Override
     protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession keycloakSession, ClientSessionContext clientSessionCtx) {
         log.tracef("Mapping user agent info to claim '%s'", mappingModel.getConfig().get(OIDCAttributeMapperHelper.TOKEN_CLAIM_NAME));
-        DeviceRepresentationProvider deviceRepresentationProvider = keycloakSession.getProvider(DeviceRepresentationProvider.class);
-        OIDCAttributeMapperHelper.mapClaim(token, mappingModel, deviceRepresentationProvider.deviceRepresentation());
+        DeviceRepresentation deviceRep = DeviceActivityManager.getCurrentDevice(userSession);
+        if (deviceRep != null) {
+            LoginRecord.Device device = LoginRecord.Device.fromDeviceRepresentation(deviceRep);
+            OIDCAttributeMapperHelper.mapClaim(token, mappingModel, device);
+        } else {
+            log.warn("No device information attached to the user session");
+        }
     }
 
     @Override

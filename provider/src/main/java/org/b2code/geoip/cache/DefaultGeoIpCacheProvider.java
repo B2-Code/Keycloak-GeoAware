0fad8363fa54f963cd74f0763b220b38008716b3
BenLad17 <48922322+BenLad17@users.noreply.github.com>
2025-11-14 18:22:48 +0100

    fix: caching config

--- provider/src/main/java/org/b2code/geoip/cache/DefaultGeoIpCacheProvider.java
+++ provider/src/main/java/org/b2code/geoip/cache/DefaultGeoIpCacheProvider.java
@@ -9,6 +9,7 @@ import org.keycloak.common.util.Time;
 import org.keycloak.models.KeycloakSession;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 @JBossLog
@@ -16,6 +17,7 @@ import java.util.Optional;
 public class DefaultGeoIpCacheProvider implements GeoIpCacheProvider {
 
     private final KeycloakSession session;
+    private final int cacheDurationMinutes;
 
     @Override
     public void put(KeycloakSession session, String ipAddress, GeoIpInfo geoIpInfo) {
@@ -26,7 +28,7 @@ public class DefaultGeoIpCacheProvider implements GeoIpCacheProvider {
     public Optional<GeoIpInfo> get(KeycloakSession session, String ipAddress) {
         LoginRecordRepository loginRecordRepository = this.session.getProvider(LoginRecordRepository.class);
         log.tracef("Getting '%s' from cache", ipAddress);
-        Instant afterTime = Instant.ofEpochMilli(Time.currentTimeMillis());
+        Instant afterTime = Instant.ofEpochMilli(Time.currentTimeMillis()).minus(cacheDurationMinutes, ChronoUnit.MINUTES);
         return loginRecordRepository.findByIpAndTimestampAfter(ipAddress, afterTime).map(LoginRecordEntity::getGeoIpInfo);
     }
 
--- provider/src/main/java/org/b2code/geoip/cache/DefaultGeoIpCacheProviderFactory.java
+++ provider/src/main/java/org/b2code/geoip/cache/DefaultGeoIpCacheProviderFactory.java
@@ -11,14 +11,23 @@ import org.keycloak.models.KeycloakSessionFactory;
 @AutoService(GeoIpCacheProviderFactory.class)
 public class DefaultGeoIpCacheProviderFactory extends ServerInfoAwareFactory implements GeoIpCacheProviderFactory {
 
+    private static final String DURATION_CONFIG_KEY = "cacheDurationMinutes";
+    private static final int DEFAULT_DURATION_DEFAULT = 60;
+
+    private Config.Scope config;
+
     @Override
     public DefaultGeoIpCacheProvider create(KeycloakSession keycloakSession) {
-        return new DefaultGeoIpCacheProvider(keycloakSession);
+        return new DefaultGeoIpCacheProvider(keycloakSession, getCacheDurationMinutes());
+    }
+
+    private int getCacheDurationMinutes() {
+        return config.getInt(DURATION_CONFIG_KEY, DEFAULT_DURATION_DEFAULT);
     }
 
     @Override
     public void init(Config.Scope scope) {
-        // NOOP
+        this.config = scope;
     }
 
     @Override

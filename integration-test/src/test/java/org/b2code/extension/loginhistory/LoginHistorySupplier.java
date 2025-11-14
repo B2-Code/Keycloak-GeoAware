f488ef69004f2a68d818c36a68288146fc884825
Benedikt Rohlf <benedikt.rohlf@gmx.de>
2025-11-14 21:30:48 +0100

    Add debug logs for database configuration in LoginHistorySupplier

--- integration-test/src/test/java/org/b2code/extension/loginhistory/LoginHistorySupplier.java
+++ integration-test/src/test/java/org/b2code/extension/loginhistory/LoginHistorySupplier.java
@@ -1,5 +1,6 @@
 package org.b2code.extension.loginhistory;
 
+import lombok.extern.slf4j.Slf4j;
 import org.keycloak.testframework.database.TestDatabase;
 import org.keycloak.testframework.injection.InstanceContext;
 import org.keycloak.testframework.injection.LifeCycle;
@@ -8,11 +9,16 @@ import org.keycloak.testframework.injection.Supplier;
 
 import java.util.Map;
 
+@Slf4j
 public class LoginHistorySupplier implements Supplier<LoginHistory, InjectLoginHistory> {
 
     @Override
     public LoginHistory getValue(InstanceContext<LoginHistory, InjectLoginHistory> instanceContext) {
         Map<String, String> config = instanceContext.getDependency(TestDatabase.class).serverConfig();
+        log.debug("Using database: {}", config.get("db-url"));
+        log.debug("Using username: {}", config.get("db-username"));
+        log.debug("Using password: {}", config.get("db-password"));
+
         return new LoginHistory(config.get("db-url"), config.get("db-username"), config.get("db-password"));
     }
 

9b1278cf760de6ac51a71b604b7468f655a5ef38
BenLad17 <48922322+BenLad17@users.noreply.github.com>
2026-03-22 14:27:22 +0100

    Add logging for missing user in GeoAwareConditionalAuthenticator condition check

--- provider/src/main/java/org/b2code/authentication/base/GeoAwareConditionalAuthenticator.java
+++ provider/src/main/java/org/b2code/authentication/base/GeoAwareConditionalAuthenticator.java
@@ -1,6 +1,7 @@
 package org.b2code.authentication.base;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.jbosslog.JBossLog;
 import org.b2code.authentication.base.condition.AuthenticatorCondition;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.authenticators.conditional.ConditionalAuthenticator;
@@ -8,6 +9,7 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
 
+@JBossLog
 @RequiredArgsConstructor
 public class GeoAwareConditionalAuthenticator implements ConditionalAuthenticator {
 
@@ -15,6 +17,9 @@ public class GeoAwareConditionalAuthenticator implements ConditionalAuthenticato
 
     @Override
     public boolean matchCondition(AuthenticationFlowContext context) {
+        if (requiresUser() && context.getUser() == null) {
+            log.warnf("User is required for condition '%s' but no user found in context", condition.getLabel());
+        }
         return condition.check(context.getSession());
     }
 
